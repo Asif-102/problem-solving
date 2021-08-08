c66bcfc3fd528f4c801e4143c8f6889bd8481695
Asif2535 <aiqbal517@gmail.com>
2021-08-08 19:35:25 +0600

    count duplicate numbers

--- find-duplicate-number.js
+++ find-duplicate-number.js
@@ -1,18 +1,19 @@
-const numbersArray = [4,3,6,3,4,3];
+const numbersArray = [4, 3, 6, 3, 4, 3];
 
-function findDuplicate(numbers){
+function findDuplicate(numbers) {
     let numbersCount = {};
 
-    for(let i=0; i<numbers.length; i++){
+    for (let i = 0; i < numbers.length; i++) {
 
-        if(numbersCount[numbers[i]]!=undefined){
+        if (numbersCount[numbers[i]] != undefined) {
             numbersCount[numbers[i]]++;
         }
-        else{
-            numbersCount[numbers[i]]=1;
+        else {
+            numbersCount[numbers[i]] = 0;
         }
     }
     return numbersCount;
 }
 
 console.log(findDuplicate(numbersArray));
+// duplicate numbers = { '3': 2, '4': 1, '6': 0 }
